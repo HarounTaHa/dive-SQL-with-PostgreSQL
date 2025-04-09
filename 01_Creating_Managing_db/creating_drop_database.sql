5f2ae6f31de912a6d12793ad433aa1919611253e
HarounTaHa <harountaha@outlook.sa>
2025-04-09 15:21:04 +0200

    create_drop_database

---
+++ 01_Creating_Managing_db/creating_drop_database.sql
@@ -0,0 +1,26 @@
+-- Access the Container's Shell
+docker exec -it inventory_db sh
+
+-- Connect to PostgreSQL Using psql
+psql -U postgres
+
+-- Connects to a database.
+\c inventory;
+
+-- Create new database
+CREATE DATABASE my_database;
+
+-- Lists all databases in PostgreSQL
+\l
+
+SELECT datname FROM pg_database;
+
+-- Deleting All Data in a Database
+TRUNCATE TABLE table_name CASCADE;
+
+
+-- Drop Database
+DROP DATABASE my_database;
+
+-- Check DB has been removed
+SELECT datname FROM pg_database;
